9e06048ec26fb99b2a89fa8b065a45f8c0019e2e
carterduke <89323630+carterduke@users.noreply.github.com>
2021-09-08 10:50:18 -0500

    classwork 9/8/21

--- Assignment-4/js/app.js
+++ Assignment-4/js/app.js
@@ -2,5 +2,12 @@ $('#btnTest').click(function() {
     const decTaxRate = .0925;
     let decHours = $('#txtHours').val();
     let decRate = $('#txtPayRate').val();
-    alert(decHours * decRate);
+    console.log(decHours * decRate);
 });
+$('#cboEmployeeType').change(function() {
+    if($('#cboEmployeeType').val() == 'FULL'){
+        $('#divHours').slideUp();
+        //$('#divHours').css('display','none');
+
+    }
+})
